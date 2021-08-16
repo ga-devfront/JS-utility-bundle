c26f759f6f0ac07cdaf6589dca1660869cf8fbc8
Alexis <alexis.guyomar@smartbooster.io>
2021-08-16 08:23:11 +0200

    update filters + add regexps and update checkers

--- checkers.js
+++ checkers.js
@@ -1,7 +1,74 @@
 import {
-    EMAIL
-} from './regexs'
+    EMAIL,
+    NAME,
+    USER_NAME,
+    TELEPHONE_BASIC,
+    TELEPHONE_INTERNATIONAL,
+    WEB_URL,
+    POSTAL_ADDRESS,
+    EMPTY,
+    NUMBER
+} from './regexps'
 
-const checkEmail = (email) => {
-    return EMAIL.test(email)
+// Global Methods //
+const checkRegexp = (regexp, value) => {
+    const myRegexp = new RegExp(regexp)
+    return myRegexp.test(value)
+}
+
+// Specific Methods //
+const isValidEmail = (email) => {
+    return checkRegexp(EMAIL, email)
+}
+
+const isValidUrl = (url) => {
+    return checkRegexp(WEB_URL, url)
+}
+
+const isValidPostalAdress = (postalAdress) => {
+    return checkRegexp(POSTAL_ADDRESS, postalAdress)
+}
+
+const isValidNumber = (number) => {
+    return checkRegexp(NUMBER, number)
+}
+
+const isValidName = (name) => {
+    return checkRegexp(NAME, name)
+}
+
+const isValidUserName = (userName) => {
+    return checkRegexp(USER_NAME, userName)
+}
+
+const isValidBasicPhone = (number) => {
+    return checkRegexp(TELEPHONE_BASIC, number)
+}
+
+const isValidInternationalPhone = (number) => {
+    return checkRegexp(TELEPHONE_INTERNATIONAL, number)
+}
+
+// Test if a string is empty or contains only spaces //
+const isBlankString = (string) => {
+    return string === null || string === 0 || checkRegexp(EMPTY, string)
+}
+
+// Test if the error code is 4XX or other //
+const is4XXError = (error) => {
+    const errorToString = error.toString()
+    return (errorToString[0] === '4' && errorToString.length === 3)
+}
+
+export {
+    isValidEmail,
+    isValidUrl,
+    isValidPostalAdress,
+    isValidNumber,
+    isBlankString,
+    is4XXError,
+    isValidName,
+    isValidUserName,
+    isValidBasicPhone,
+    isValidInternationalPhone
 }
--- filters.js
+++ filters.js
@@ -1,24 +1,26 @@
-const format_price = (value) => {
+function formatPrice(value) {
     if (value) {
         return new Intl.NumberFormat().format(value) + ' €';
     }
-    return 'no defined'
+    return ''
 }
 
-const format_date = (date) => {
+function formatDate(date) {
     if (date) {
         const options = { weekday: 'long', month: 'long', day: 'numeric' };
         const newDate = new Date(date)
         return newDate.toLocaleDateString("fr-FR", options);
     }
-    return 'no defined'
+    return ''
 }
 
-const format_date_time = (date) => {
+function formatDateTime(date) {
     if (date) {
         const options = { weekday: 'long', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
         const newDate = new Date(date)
         return newDate.toLocaleDateString("fr-FR", options);
     }
-    return 'no defined'
+    return ''
 }
+
+export { formatPrice, formatDate, formatDateTime }
